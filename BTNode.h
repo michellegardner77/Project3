da45ab995f210caa9bc21d772ec92d73b492ec75
Michelle <mgardner1994@gmail.com>
2016-07-21 13:31:35 -0500

    Create BTNode.h
    I didn't add our names yet.
    
    I got this from the Source Code from Blackboard.

---
+++ BTNode.h
@@ -0,0 +1,41 @@
+#ifndef BTNODE_H_
+#define BTNODE_H_ 
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+/*BTNode.h is from the Source Code on Blackboard*/
+
+
+
+// A node for Binary Tree
+template<typename Item_Type>
+struct BTNode
+{
+	// Data Fields
+	Item_Type data;
+	BTNode<Item_Type>* left;
+	BTNode<Item_Type>* right;
+	
+	// Constructor
+	BTNode(const Item_Type& the_data,
+		BTNode<Item_Type>* left_val = NULL,
+		BTNode<Item_Type>* right_val = NULL) :
+		data(the_data), left(left_val), right(right_val) {}
+
+	// Destructor (to avoid warning message)
+	virtual ~BTNode() {}
+
+	// to_string
+	virtual string to_string() const { ostringstream os; os << data; return os.str(); }
+
+}; // End BTNode
+
+// Overloading the ostream insertion operator
+template<typename Item_type>
+ostream& operator<<(ostream& out, const BTNode<Item_Type>& node)
+{
+	return out << node.to_string();
+}
+
+#endif
